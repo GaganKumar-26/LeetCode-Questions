3136ed1bae52ca27ce217615d8ffdea4d7299eb9
GaganKumar-26 <146158851+GaganKumar-26@users.noreply.github.com>
2024-07-07 11:24:43 +0530

    Add files via upload

---
+++ LeetCode7.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+#include <limits.h>
+using namespace std;
+
+int main(){
+
+    int x;
+    cin>>x;
+
+    int ans = 0;
+
+    while(x != 0){
+        int digit = x % 10;
+
+        if((ans > INT_MAX/10) || (ans < INT_MIN/10)){
+            cout<<" 0 "<< endl;
+            return 0;
+        }
+        ans = (ans * 10) + digit;
+        x = x / 10;
+    }
+    cout << ans << endl;
+    return ans;
+}
