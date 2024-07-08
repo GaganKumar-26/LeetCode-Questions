eddbe5ff6145952023da4c7e2d37bca69ce33a7c
GaganKumar-26 <146158851+GaganKumar-26@users.noreply.github.com>
2024-07-08 17:04:40 +0530

    Add files via upload

---
+++ LeetCode1009.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>> n;
+
+    int m = n;
+    int mask = 0;
+
+    while(m!=0){
+
+        mask = (mask << 1) | 1;
+        m = m >> 1;
+
+    }
+
+    int ans = (~n) & mask;
+    cout << ans << endl;
+    return ans;
+}
